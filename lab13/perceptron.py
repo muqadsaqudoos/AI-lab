d4e2b7750b2b47fd6f7aecc5a5f9b48b2ba7f6bd
Muqadsa Qudoos <muqadsaqudoos@gmail.com>
2025-01-10 09:55:54 +0500

    lab13

--- lab13/perceptron.py
+++ lab13/perceptron.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neural_network import MLPClassifier
@@ -6,10 +7,13 @@ from sklearn.linear_model import Perceptron
 def plot_decision_boundary(X, y, model, title):
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    
     xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01),
                          np.arange(y_min, y_max, 0.01))
+    
     Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
+    
     plt.contourf(xx, yy, Z, alpha=0.8, cmap=plt.cm.Paired)
     plt.scatter(X[:, 0], X[:, 1], c=y, edgecolor='k', s=20, cmap=plt.cm.Paired)
     plt.title(title)
@@ -17,17 +21,25 @@ def plot_decision_boundary(X, y, model, title):
 
 def perceptron_task():
     X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    y = np.array([0, 0, 0, 1]) 
+    y = np.array([0, 0, 0, 1])  
+
     model = Perceptron(max_iter=1000, eta0=0.1, random_state=0)
-    model.fit(X, y)
+    model.fit(X, y)  
+
     plot_decision_boundary(X, y, model, title="Perceptron Decision Boundary")
 
 def xor_nn_task():
     X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    y = np.array([0, 1, 1, 0])  
+    y = np.array([0, 1, 1, 0]) 
 
-    model = MLPClassifier(hidden_layer_sizes=(4,), max_iter=5000, learning_rate_init=0.1, random_state=0)
-    model.fit(X, y)
+    model = MLPClassifier(
+        hidden_layer_sizes=(4,),  
+        activation='relu',       
+        max_iter=5000,          
+        learning_rate_init=0.1, 
+        random_state=0          
+    )
+    model.fit(X, y) 
 
     plot_decision_boundary(X, y, model, title="Neural Network Decision Boundary for XOR")
 
